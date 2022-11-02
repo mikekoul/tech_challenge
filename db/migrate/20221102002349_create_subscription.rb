e66547af089e8a20e515bf8faba6f3c5a69af1af
Michael Koulouvaris <mikekoul@gmail.com>
2022-11-01 18:26:39 -0600

    Add database schema'

---
+++ db/migrate/20221102002349_create_subscription.rb
@@ -0,0 +1,14 @@
+class CreateSubscription < ActiveRecord::Migration[5.2]
+  def change
+    create_table :subscriptions do |t|
+      t.string :title
+      t.string :price
+      t.integer :status
+      t.string :frequency
+      t.references :tea, foreign_key: true
+      t.references :customer, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
